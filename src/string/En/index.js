bcd4924fda49c2c6a3c1872c5d6b111d9c3143c3
feryadialoi <37759681+feryadialoi@users.noreply.github.com>
2019-08-25 23:26:43 +0700

    debug

--- src/string/En/index.js
+++ src/string/En/index.js
@@ -5,4 +5,4 @@ export const hello =
   ", and I'm Pontianak-Indonesia based junior programmer";
 
 export const text2 =
-  "I have diverse set of skills, ranging from design, to HTML + CSS + JavaScript";
+  "I have diverse set of skills, ranging from design, to HTML + CSS + JavaScript, mobile app dev.";
